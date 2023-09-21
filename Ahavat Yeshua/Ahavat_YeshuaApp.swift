e409ee1f8b508ce0067101a081f50e68e84aabc0
ilia Pavlov <68978403+ilia-pavlov@users.noreply.github.com>
2023-09-20 20:59:23 -0400

    Initial Commit

---
+++ Ahavat Yeshua/Ahavat_YeshuaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ahavat_YeshuaApp.swift
+//  Ahavat Yeshua
+//
+//  Created by Ilia Pavlov on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Ahavat_YeshuaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ahavat Yeshua/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ahavat Yeshua
+//
+//  Created by Ilia Pavlov on 9/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
