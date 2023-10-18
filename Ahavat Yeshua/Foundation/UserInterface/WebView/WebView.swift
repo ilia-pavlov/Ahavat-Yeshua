2220cc62e858043b69903bcccd89c71deeefcad6
ilia Pavlov <68978403+ilia-pavlov@users.noreply.github.com>
2023-10-17 23:06:30 -0400

    added loader

--- Ahavat Yeshua/Foundation/UserInterface/WebView/WebView.swift
+++ Ahavat Yeshua/Foundation/UserInterface/WebView/WebView.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct WebView: View {
+    @State private var isLoading = true
     let url: String
 
     var body: some View {
-        NavigationView {
+        ZStack {
             let urlFormat: URL = URL(string: url)!
             WebViewRepresentable(urlString: urlFormat.absoluteString)
                 .navigationBarTitleDisplayMode(.inline)
+        }
+        
+        if isLoading {
+            VStack {
+                Spacer()
+                ProgressView("Loading...")
+                    .progressViewStyle(.circular)
+                    .foregroundStyle(.gray)
+                    .scaleEffect(2.0, anchor: .center)
+                    .onAppear {
+                        Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { _ in
+                            isLoading = false
+                        }
+                    }
+                Spacer()
+            }
         }
     }
 }
